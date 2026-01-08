fb3e82182df1282d5bf95421f087ac6ee186097c
benjamin-trt <torrenti.b123@gmail.com>
2026-01-08 14:46:13 +0100

    Add files via upload

---
+++ crazy_racer.py
@@ -0,0 +1,21 @@
+import sys
+import pygame
+
+pygame.init()
+taille_fenetre = (1300,800)
+ecran_du_jeu = pygame.display.set_mode(taille_fenetre)
+pygame.display.set_caption("Crazy Racer")
+couleur_fond = ('aquamarine4')
+debut_jeu = True
+
+while debut_jeu == True :
+    evenement = pygame.event.get()
+    for e in evenement :
+        if e.type == pygame.QUIT :
+            debut_jeu = False
+
+    ecran_du_jeu.fill(couleur_fond)
+    pygame.display.update()
+
+pygame.quit()
+sys.exit()
