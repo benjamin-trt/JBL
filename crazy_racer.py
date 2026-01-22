d0660b038b31959cd4abc2f19af9af36bd69a5bf
jules2017 <julesmc06@gmail.com>
2026-01-22 14:46:32 +0100

    Update crazy_racer.py

--- crazy_racer.py
+++ crazy_racer.py
@@ -2,6 +2,13 @@ import sys
 import pygame
 
 pygame.init()
+pygame.mixer.init()
+son_perdu = pygame.mixer.Sound("bruit_fin.wav") 
+son_perdu.set_volume(0.5)
+son_moteur = pygame.mixer.Sound("bruit_moteur.wav") 
+son_moteur.set_volume(0.7)
+son_menu = pygame.mixer.Sound("son_menu.wav") 
+son_moteur.set_volume(0.7)
 
 taille_fenetre = (1300, 800)
 ecran_du_jeu = pygame.display.set_mode(taille_fenetre)
@@ -30,6 +37,7 @@ route = pygame.Rect(350, 0, 600, 800)
 
 clock = pygame.time.Clock()
 en_cours = True
+son_perdu_joue = True
 
 while en_cours:
     for e in pygame.event.get():
@@ -41,14 +49,17 @@ while en_cours:
                 etat = JEU
                 voiture.x = 635
                 voiture.y = 350
+                son_menu.play()
 
         if e.type == pygame.KEYDOWN:
             if e.key == pygame.K_z:
                 etat = ACCUEIL
-
+            if e.key == pygame.K_a:
+                en_cours = FALSE
             if etat == JEU:
                 if e.key == pygame.K_ESCAPE:
                     etat = FIN
+                    
 
     touches = pygame.key.get_pressed()
 
@@ -59,18 +70,24 @@ while en_cours:
             voiture.x += vitesse
         if touches[pygame.K_UP]:
             voiture.y -= vitesse
+            son_moteur.play()
         if touches[pygame.K_DOWN]:
             voiture.y += vitesse
 
        
         if voiture.left < route.left:
             voiture.left = route.left
+            son_perdu.play()
+            etat = FIN
         if voiture.right > route.right:
             voiture.right = route.right
+            son_perdu.play()
         if voiture.top < route.top:
+            son_perdu.play()
             voiture.top = route.top
         if voiture.bottom > route.bottom:
             voiture.bottom = route.bottom
+            son_perdu.play()
 
     if etat == ACCUEIL:
         ecran_du_jeu.fill(NOIR)
@@ -91,8 +108,10 @@ while en_cours:
         ecran_du_jeu.fill(NOIR)
         texte_fin = police_titre.render("COURSE TERMINÉE", True, BLANC)
         texte_retour = police_texte.render("Appuie sur Z pour retourner au menu", True, BLANC)
+        texte_sortie = police_texte.render("Appuie sur a pour sortir du jeu", True, BLANC)
         ecran_du_jeu.blit(texte_fin, (330, 300))
         ecran_du_jeu.blit(texte_retour, (390, 430))
+        ecran_du_jeu.blit(texte_sortie, (390, 470))
 
     pygame.display.update()
     clock.tick(60)
