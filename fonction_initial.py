300be1056e5ed232224070c0b5ebd4d107675077
jules2017 <julesmc06@gmail.com>
2026-02-05 14:40:10 +0100

    Update fonction_initial.py

--- fonction_initial.py
+++ fonction_initial.py
@@ -1,6 +1,7 @@
 import pygame
 
 def init(son_menu):
+    """Cette fonction renvoie tout les éléments du jeu qui ne changent pas et qui ont besoin d'être réinitialisés au début de chaque partie."""
     voiture = pygame.Rect(635, 350, 30, 60)
     voiture.x == 635
     voiture.y == 350
