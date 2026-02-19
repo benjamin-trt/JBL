7a0c38af937fd73f847409b744c3202d8bf931c9
jules2017 <julesmc06@gmail.com>
2026-02-19 15:52:28 +0100

    j'ai fait en sorte que des voiturees arrivent aleatoirement d'en face

--- crazy_racer.py
+++ crazy_racer.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import random
 from fonctions.fonction_ouverture import accueil    # Fonction qui affiche l'écran d'accueil
 from fonctions.fonction_fin import fin              # Fonction qui affiche l'écran de fin
 from fonctions.fonction_initial import init         # Fonction qui initialise la partie
@@ -58,6 +59,16 @@ nb_voies = 4
 largeur_voie = route.width // nb_voies
 route_offset = 0        
 
+centres_voies = [
+    route.left + largeur_voie // 2 + i*largeur_voie
+    for i in range(nb_voies)
+]
+
+voitures_adverses = []
+largeur_voiture = 30
+hauteur_voiture = 60
+delai_spawn = 60
+compteur_spawn = 0
 while en_cours:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
@@ -84,11 +95,42 @@ while en_cours:
         ecran_du_jeu.fill(BLEU_CIEL)                      # Dessin de la mer
         pygame.draw.rect(ecran_du_jeu, GRIS, route)       # Dessin de la route
         
+        compteur_spawn += 1
+
+        if compteur_spawn >= delai_spawn:
+            # Nombre de voitures à créer (1 à 3 par exemple)
+            nb_voitures_spawn = random.randint(1, 3)
+
+            for _ in range(nb_voitures_spawn):
+                voie = random.randint(0, nb_voies - 1)
+                x = centres_voies[voie] - largeur_voiture // 2
+
+                voitures_adverses.append(
+                    pygame.Rect(x, -hauteur_voiture, largeur_voiture, hauteur_voiture)
+                )
+
+            # Nouveau délai aléatoire à chaque spawn
+            delai_spawn = random.randint(30, 80)
+            compteur_spawn = 0
+
         # Dessin des lignes de séparation des voies de la route
         for i in range(1, nb_voies):
             x = route.left + i * largeur_voie
             for y in range(-40, 800, 40):
                 pygame.draw.rect(ecran_du_jeu, BLANC, (x - 2, y + route_offset, 4, 20))
+                
+        # Déplacement et affichage des voitures adverses
+        for v in voitures_adverses[:]:
+            v.y += 5   # vitesse des adversaires
+
+            if v.top > 800:
+                voitures_adverses.remove(v)
+
+            if voiture.colliderect(v):
+                son_perdu.play()
+                etat = FIN
+
+            pygame.draw.rect(ecran_du_jeu, NOIR, v)
 
         # Déplacements de la voiture
         if touches[pygame.K_LEFT]: 
