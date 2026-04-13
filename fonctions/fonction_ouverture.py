854a5cb767ed9c74260d4433761304e6411ae93b
benjamin-trt <torrenti.b123@gmail.com>
2026-04-13 15:59:20 +0200

    jai modifie la fonction pour faire afficher le skin au debut dans l accueil

--- fonctions/fonction_ouverture.py
+++ fonctions/fonction_ouverture.py
@@ -1,6 +1,5 @@
 import pygame
-
-def accueil(ecran_du_jeu, police_titre, police_texte, police_texte1, BLANC, BLANC1, GRIS1, INDIGO, ROUGE, GRIS, DORE, bouton_jouer, son_menu):
+def accueil(ecran_du_jeu, police_titre, police_texte, police_texte1, BLANC, BLANC1, GRIS1, INDIGO, ROUGE, GRIS, DORE, bouton_jouer, son_menu, skins, index_skin):
     """Cette fonction renvoie l'écran d'accueil du jeu, et permet au joueur de lancer sa partie."""
     image_accueil = pygame.image.load("images/villevoiture.png")
     image_accueil = pygame.transform.scale(image_accueil, ecran_du_jeu.get_size())
@@ -18,5 +17,9 @@ def accueil(ecran_du_jeu, police_titre, police_texte, police_texte1, BLANC, BLAN
     pygame.draw.rect(ecran_du_jeu, BLANC1, bouton_jouer, border_radius=10)
     texte_jouer = police_texte1.render("JOUER", True, GRIS)
     ecran_du_jeu.blit(texte_jouer, (bouton_jouer.x + 25, bouton_jouer.y + 25))
+    image_preview = pygame.image.load(skins[index_skin]).convert_alpha()
+    image_preview = pygame.transform.scale(image_preview, (120, 240))
+    ecran_du_jeu.blit(image_preview, (150, 475))
+    
     if not son_menu.get_num_channels():
         son_menu.play(-1)
